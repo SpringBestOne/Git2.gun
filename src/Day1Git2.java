790dfed4d512e4ebedbf73d41557eed22efa96e6
Spring Stone <springbestone@gmail.com>
2022-08-15 22:11:22 +0300

    V5

---
+++ src/Day1Git2.java
@@ -0,0 +1,25 @@
+public class Day1Git2 {
+    public static void main(String[] args) {
+        System.out.println("Git 2. class");
+
+        /*
+        --------Komutlar
+git --version
+git config --global user.email "email_adresiniz"
+git config --global user.name "isminiz"
+git config --global -l
+git init
+git status
+git add .
+git status
+git diff --> Working space deki değişikliği gösterir
+git diff --staged --> Staging Area daki değişiklikleri gösterir
+git commit -m "first commit"
+git show "hashcode" --> Versiyondaki değişiklikleri gösterir
+git log --oneline
+git remote add origin https://github.com/techproed2020/Git_...
+git push -u origin master
+         */
+
+    }
+}
---
+++ src/Day2Git.java
@@ -0,0 +1,20 @@
+public class Day2Git {
+    public static void main(String[] args) {
+        /*
+        1) git init  --> Local Repo olusturmak için yani git ile klasorumuzun içindeki dosyaları ilişkilendirmek için kullanılır.
+        2) git add . --> Working space den (yani yerel) dosyalarımı stageing area ya (yani commitlemek) için beklenen yere gönderir
+        3) git status --> Working space deki ve ataging area da ki durumu gösterir
+        4) git commit  -m "mesaj"  --> Staging area dan commit stora dosyaşarını göndermek için kullanılır
+        (coomit yani versiyon yani surum olusturmus olurum)
+        5) git push --> Uzak repoya (yani remote  - Github ) göndermek için kullandığımız kod
+        Yalnız git push komutunu direkt kullanmak istersek 1 kez
+                            git remote add origin - adress-
+                            git push -u origin master
+
+         Not : Bu iki komutu tek seferde kullandıktan sonra ikinci commitlerim için sadece git push kullanırız.
+
+        Not  git log --oneline  --> commitlerimdeki yani versiyonlarımdaki durumu gösterir...
+         */
+        System.out.println("Github ı da ogrendim");
+    }
+}
